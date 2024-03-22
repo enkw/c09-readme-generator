2114714b391bd321af77b1fcb0f7051ecf079eef
enkw <iankwolfe@gmail.com>
2024-03-21 22:29:08 -0400

    Added questions array to index

--- index.js
+++ index.js
@@ -3,7 +3,63 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project:'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions if needed:'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage information:'
+      },
+      {
+        type: 'input',
+        name: 'credits',
+        message: 'Please list credits for this project:'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please select the license used:'
+      },
+      {
+        type: 'input',
+        name: 'features',
+        message: 'Please list your project features:'
+      },
+      {
+        type: 'input',
+        name: 'contribute',
+        message: 'How can others contribute to your project if needed?'
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide testing instructions if needed:'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
